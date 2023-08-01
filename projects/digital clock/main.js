4144c353c0860c3e0eca4f3baeeef4c0f54eedf1
yalgar0 <dk.yalgar@gmail.com>
2023-08-01 22:22:09 +0500

    learn about button ripples effect

--- projects/digital clock/main.js
+++ projects/digital clock/main.js
@@ -15,7 +15,10 @@ function updateClock(){
     ampm = "PM";
  }
 
-
+  
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
   hourE1.innerText = h;
   minuteE1.innerText = m;
---
+++ projects/mutiplication app/index.js
@@ -0,0 +1,33 @@
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+const questionE1 = document.getElementById("question");
+const formE1 = document.getElementById('form');
+const inputE1 = document.getElementById('input');
+const scoreE1 = document.getElementById('score');
+let score = JSON.parse(localStorage.getItem("score"));
+if(!score){
+    score=0;
+}
+scoreE1.innerHTML = `score: ${score}`
+questionE1.innerText = `What is ${num1} mulitiply ${num2}?`;
+
+
+const correctAns = num1 * num2;
+
+
+
+formE1.addEventListener("submit",()=>{
+ const userAns = +inputE1.value
+ if(userAns == correctAns){
+    score +=1;
+    updateLocalStorage();
+
+ }else{
+    score -=1;
+    updateLocalStorage();
+ } 
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
